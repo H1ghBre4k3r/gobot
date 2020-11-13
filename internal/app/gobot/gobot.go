69ca42276a170d39ce5e0203bf9f34e2396f16f4
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2020-11-13 01:01:03 +0100

    Add very important comment

--- internal/app/gobot/gobot.go
+++ internal/app/gobot/gobot.go
@@ -39,6 +39,7 @@ func Gobot(Token string) {
 	discord.Close()
 }
 
+// Handle messages.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
